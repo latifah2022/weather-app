80da68fad192483c61ceb5afd05cab36137f8f68
Latifah Zawedde <zawedde2021@gmail.com>
2022-08-30 22:58:50 -0700

    3-commit

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 //determine the variables 
 //var weather
 var inputval = document.querySelector('#cityinput')
-var btn = document.querySelector('#add');
+var btn = document.querySelector('#btn');
 var city = document.querySelector('#cityoutput')
 var descrip = document.querySelector('#humidity')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 var wind = document.querySelector('#index')
 
+var date = moment();
+$("#currentDay").text(date.format('dddd, MMM Do YYYY,  h:mm:ss, a'));
+
+// function timeTracker() {
+//     //get current number of hours.
+//     var timeNow = moment().hour();
+// }
 
 
-function weath() {
-    
+function weather();
+
+weather.temperature = {
+    unit : "celsius"
 }
+
 
 function cities() {
 
