155250ee812a1dee27c992f884f758dd3fbbd316
Latifah Zawedde <zawedde2021@gmail.com>
2022-08-30 18:29:34 -0700

    2-commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+//determine the variables 
+//var weather
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#humidity')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var wind = document.querySelector('#index')
+
+
+
+function weath() {
+    
+}
+
+function cities() {
+
+}
+
+function forecast() {
+
+}
+
+//addEventListener
