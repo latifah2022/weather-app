e6ddf854e634cdee5f40fd61e22df3d2ef7bf99c
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-06 19:10:25 -0700

    3commit

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //determine the variables 
-var weather = document.querySelector('header')
+var weatherEl = document.querySelector('header')
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#btn');
 var city = document.querySelector('#cityoutput')
@@ -7,17 +7,26 @@ var descrip = document.querySelector('#humidity')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 var wind = document.querySelector('#index')
+var city;
+var APIKey = "12341234123412341234123412341234";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-var date = moment();
-$("#currentDay").text(date.format('dddd, MMM Do YYYY,  h:mm:ss, a'));
+function timeTracker() {
+  var date = moment().format('dddd, MMM Do YYYY,  h:mm:ss, a');
+  //$("#currentDay").text(date.format('dddd, MMM Do YYYY,  h:mm:ss, a'));
+  //moment().format('MMMM Do YYYY, h:mm:ss a');
+  weatherEl.text(date);
+}
 
-// function timeTracker() {
-//     //get current number of hours.
-//     var timeNow = moment().hour();
-// }
+function timeTracker() {
+    var timeNow = moment().hour();
+ }
 
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-function weather();
+function weather() {
+
+}
 
 weather.temperature = {
     unit : "celsius"
